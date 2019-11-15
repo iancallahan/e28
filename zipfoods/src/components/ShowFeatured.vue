9d06c5a81b6a942370dd2fb5faeeef46b2afb5ba
Ian Callahan <ian.callahan@gmail.com>
2019-11-14 23:06:54 -0500

    move data to root component

---
+++ zipfoods/src/components/ShowFeatured.vue
@@ -0,0 +1,28 @@
+<!-- src/components/ShowFeatured.vue -->
+<template>
+    <div id='featured'>
+        <h2>Featured Products</h2>
+        <ul class='cleanList'>
+            <li v-for='product in featuredProducts' :key='product.id'>{{ product.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowFeatured',
+  computed: {
+    featuredProducts: function() {
+    function isMatch(product) {
+        return product.categories.includes(this);
+    }
+    return this.products.filter(isMatch, this.category);
+}},
+props: {
+    category: String,
+    products: Array
+  }
+
+  }
+
+</script>
