26cdbbae6143ee39dcaacc13605426e905892a0b
Ian Callahan <ian.callahan@gmail.com>
2019-11-14 22:51:17 -0500

    Initial zipfoods setup

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="ZipFoods logo" id="logo" src="./assets/images/zipfoods-logo.png">
+    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <show-products></show-products>
+  </div>
+</template>
+
+<script>
+import ShowProducts from  './components/ShowProducts.vue'
+export default {
+  name: 'app',
+  components: {ShowProducts
+  }
+}
+</script>
+
+<style lang='scss'>
+@import './assets/css/zipfoods.scss';
+</style>
+
